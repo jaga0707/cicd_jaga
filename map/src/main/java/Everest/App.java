7aaf6ac64317d02032f4280d27f381e387863cf8
jagan109 <jaga.a@gmail.com>
2021-03-30 22:01:14 +0530

    added new project

---
+++ map/src/main/java/Everest/App.java
@@ -0,0 +1,13 @@
+package Everest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
